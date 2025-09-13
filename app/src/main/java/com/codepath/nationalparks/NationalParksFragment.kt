a9cd7d35a6dbaf36126dd4870621a5fc3dc846aa
Conye72 <conyersmalikai@gmail.com>
2025-09-12 23:40:03 -0400

    Added git file and corresponding gradle files

--- app/src/main/java/com/codepath/nationalparks/NationalParksFragment.kt
+++ app/src/main/java/com/codepath/nationalparks/NationalParksFragment.kt
@@ -1,6 +1,7 @@
 package com.codepath.nationalparks
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,12 +10,19 @@ import androidx.core.widget.ContentLoadingProgressBar
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.RequestParams
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import okhttp3.Headers
+import org.json.JSONArray
 
 
 // --------------------------------//
 // CHANGE THIS TO BE YOUR API KEY  //
 // --------------------------------//
-private const val API_KEY = "<YOUR-API-KEY-HERE>"
+private const val API_KEY = "xL4HshVvcJK3PrCkJg41UCvRYj7GQNqXzySSKQNl"
 
 /*
  * The class for the only fragment in the app, which contains the progress bar,
@@ -48,15 +56,17 @@ class NationalParksFragment : Fragment(), OnListFragmentInteractionListener {
         progressBar.show()
 
         // Create and set up an AsyncHTTPClient() here
+        val client = AsyncHttpClient()
+        val params = RequestParams()
+        params["api_key"] = API_KEY
 
-        // Using the client, perform the HTTP request
+// Using the client, perform the HTTP request
+        client[
+            "https://developer.nps.gov/api/v1/parks",
+            params,
+            object : JsonHttpResponseHandler()
+            { // connect these callbacks to your API call
 
-        /* Uncomment me once you complete the above sections!
-        {
-            /*
-             * The onSuccess function gets called when
-             * HTTP response status is "200 OK"
-             */
             override fun onSuccess(
                 statusCode: Int,
                 headers: Headers,
@@ -66,8 +76,19 @@ class NationalParksFragment : Fragment(), OnListFragmentInteractionListener {
                 progressBar.hide()
 
                 //TODO - Parse JSON into Models
+                // Filter out the "data" JSON array and turn into a String
+                val dataJSON = json.jsonObject.get("data") as JSONArray
+                val parksRawJSON = dataJSON.toString()
 
-                val models : List<NationalPark> = mutableListOf() // Fix me!
+                // Create a Gson instance to help parse the raw JSON
+                val gson = Gson()
+
+// Tell Gson what type we’re expecting (a list of NationalPark objects)
+                val arrayParkType = object : TypeToken<List<NationalPark>>() {}.type
+
+// Convert the raw JSON string into a list of actual NationalPark data models
+                val models: List<NationalPark> = gson.fromJson(parksRawJSON, arrayParkType)
+
                 recyclerView.adapter = NationalParksRecyclerViewAdapter(models, this@NationalParksFragment)
 
                 // Look for this in Logcat:
@@ -93,7 +114,6 @@ class NationalParksFragment : Fragment(), OnListFragmentInteractionListener {
                 }
             }
         }]
-        */
 
     }
 
